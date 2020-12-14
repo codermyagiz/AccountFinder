7600c61b01424a46f60a1dcc13dbc0a59d20221a
codermyagiz <yagiz.ysk@hotmail.com>
2020-12-15 00:54:58 +0300

    Code shortened with jQuery

--- js/get.js
+++ js/get.js
@@ -1,46 +1,43 @@
 $("a").click(function() {
+  $(".back").empty();
+
   function get_username(){
     var apii = "https://instagram.com/username/?__a=1"
-    let b = document.getElementById("namehere").value;
+    let b = $("#namehere").val();
     apii = apii.replace("username", b);
     return apii;
   }
   apii = get_username()
 
   $.getJSON(apii, function(data){
-    var back = document.querySelector(".back")
 
-    var abc = document.createElement("div")
-    abc.setAttribute("class", "abc")
-    back.appendChild(abc)
+    var back_button = $("<button></button>").attr("id", "go_back").html(`<i class="fa fa-caret-left"></i>`);
+    $('.back').append(back_button)
 
-    abc.innerHTML += `<img src=${data["graphql"]["user"].profile_pic_url_hd}></img>`;
+    var abc = $("<div></div>").attr("class", "abc").html(`<img src=${data["graphql"]["user"].profile_pic_url_hd}></img>`);
+    $('.back').append(abc);
 
-    var ol = document.createElement("div")
-    ol.setAttribute("class", "overlay")
-    abc.appendChild(ol)
+    var ol = $("<div></div>").attr("class", "overlay").html(`<a href=${data["graphql"]["user"].profile_pic_url_hd}>indir</a>`);
+    $('.abc').append(ol);
 
-    ol.innerHTML += `<a href=${data["graphql"]["user"].profile_pic_url_hd}>indir</a>`;
+    var user = $("<p></p>").attr("class", "user").html(`<a id="user" href="https://instagram.com/${data["graphql"]["user"].username}">@${data["graphql"]["user"].username}</a>`);
+    $('.back').append(user);
 
-    var user = document.createElement("p")
-    user.setAttribute("class", "user")
-    back.appendChild(user)
+    var ii_follow = $("<div></div>").attr("class", "i_follow").html(`<p>Takipçi: ${data["graphql"]["user"]["edge_followed_by"].count}</p><span id="line">|</span><p id="following_text">Takip Edilen: ${data["graphql"]["user"]["edge_follow"].count}</p>`);
+    $('.back').append(ii_follow);
 
-    user.innerHTML += `<a id="user" href="https://instagram.com/${data["graphql"]["user"].username}">@${data["graphql"]["user"].username}</a>`;
+    var nblu = `<p id="name">İsim: ${data["graphql"]["user"].full_name}</p><p id="bio">Bio: ${data["graphql"]["user"].biography}</p><p id="link">Bio'daki Link:  <a href="${data["graphql"]["user"].external_url}">Link'e Git</a></p><button id="goo" onclick="window.location.href='https://instagram.com/${data["graphql"]["user"].username}'"><span>Hesaba Git</span></button>`
+    $('.back').append(nblu);
 
-    var ii_follow = document.createElement("div")
-    ii_follow.setAttribute("class", "i_follow")
-    back.appendChild(ii_follow)
-
-    ii_follow.innerHTML += `<p>Takipçi: ${data["graphql"]["user"]["edge_followed_by"].count}</p>`;
-    ii_follow.innerHTML += `<span id="line">|</span>`;
-    ii_follow.innerHTML += `<p id="following_text">Takip Edilen: ${data["graphql"]["user"]["edge_follow"].count}</p>`;
-
-    back.innerHTML += `<p>İsim: ${data["graphql"]["user"].full_name}</p>`;
-    back.innerHTML += `<p>Bio: ${data["graphql"]["user"].biography}</p>`;
-    back.innerHTML += `<p id="link">Bio'daki Link:  <a href="${data["graphql"]["user"].external_url}">Link'e Git</a></p>`;
-    back.innerHTML += `<button id="goo" onclick="window.location.href='https://instagram.com/${data["graphql"]["user"].username}'"><span>Hesaba Git</span></button>`;
-
+    if (`${data["graphql"]["user"].full_name}` == ""){
+      $("p").remove("#name")
+    }
+    if (`${data["graphql"]["user"].biography}` == "") {
+      $("p").remove("#bio")
+    }
+    if (`${data["graphql"]["user"].external_url}` == "null") {
+      $("p").remove("#link")
+    }
 
     $(".content").css({'transform': 'rotateY(180deg)', 'transition': 'transform 0.5s'});
 
